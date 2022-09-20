40a62641898bd41438c901578f590f6b57e3607a
unknown <juanpacloud@gmail.com>
2022-09-19 19:40:56 -0700

    Actualizar miscelaneo agregado

---
+++ ProyectoParcial2/resources/views/dashboard/misc/edit.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('/styles.css')}}">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <title>Actualizar Miscelaneo</title>
+</head>
+<body>
+    @include('dashboard.partials.nav-header-main')
+    <form action="{{route('misc.update',$misc->id)}}" method='post' class="form-register">
+        @method('PUT')
+        @include('dashboard.partials._formMisc')
+    </form>
+</body>
+</html>
